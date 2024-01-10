b9cfe24871cd8bffac4540fdb9c5821097e06b75
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-10 20:04:26 +0100

    component: instruction added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule, NgStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game';
-import { PlayerComponent } from '../player/player.component';
+import { PlayerComponent } from './player/player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { InstructionComponent } from './instruction/instruction.component';
 
 
 @Component({
@@ -19,7 +20,8 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
-    DialogAddPlayerComponent
+    DialogAddPlayerComponent,
+    InstructionComponent
   ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
@@ -28,6 +30,7 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   game: Game;
   currentCard: string = '';
+  hasStarted = false;
 
   constructor(public dialog: MatDialog) {
     this.game = new Game();
@@ -42,8 +45,10 @@ export class GameComponent implements OnInit {
   takeCard() {
     const lastCard = this.game.stack.pop();
     if (!this.pickCardAnimation && lastCard != undefined) {
+      this.hasStarted = true;
       this.currentCard = lastCard;
       this.pickCardAnimation = true;
+      this.incrementCurrentPlayer();
 
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
@@ -58,5 +63,13 @@ export class GameComponent implements OnInit {
     dialogRef.afterClosed().subscribe((name: string) => {
       this.game.players.push(name);
     });
+  }
+
+  incrementCurrentPlayer() {
+    if (this.game.currentPlayer < (this.game.players.length - 1)) {
+      this.game.currentPlayer++;
+    } else {
+      this.game.currentPlayer = 0;
+    }
   }
 }
--- src/app/game/player/player.component.ts
+++ src/app/game/player/player.component.ts
@@ -10,6 +10,7 @@ import { Component, Input } from '@angular/core';
 export class PlayerComponent {
 
   @Input() name = '';
+  @Input() isPlayerActive = false;
 
   constructor() {
   }
