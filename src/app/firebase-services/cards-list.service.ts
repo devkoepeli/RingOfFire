163a6af46609cda23368550278fe030630473766
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-16 21:58:27 +0100

    read fn added

--- src/app/firebase-services/cards-list.service.ts
+++ src/app/firebase-services/cards-list.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
+import { collection, Firestore, doc, documentId, onSnapshot, DocumentData } from '@angular/fire/firestore';
+import { Card } from '../interfaces/card.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,47 @@ import { Firestore } from '@angular/fire/firestore';
 export class CardsListService {
   firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  unsubCardsStack;
+  unsubCardsPlayed;
+
+  cardsStack: Card[] = [];
+  cardsPlayed: Card[] = [];
+
+  constructor() { 
+    this.unsubCardsStack = this.snapshotCardsStack();
+    this.unsubCardsPlayed = this.snapshotCardsPlayed();
+  }
+
+  snapshotCardsStack() {
+    return onSnapshot(this.getColRef('cardsStack'), cards => {
+      this.cardsStack = [];
+      cards.forEach(card => {
+        this.cardsStack.push(this.setCardObject(card.data(), card.id))
+      });
+    });
+  }
+
+  snapshotCardsPlayed() {
+    return onSnapshot(this.getColRef('cardsPlayed'), cards => {
+      this.cardsPlayed = [];
+      cards.forEach(card => {
+        this.cardsPlayed.push(this.setCardObject(card.data(), card.id));
+      });
+    });
+  }
+
+  getColRef(colId: 'cardsStack' | 'cardsPlayed') {
+    return collection(this.firestore, colId);
+  }
+
+  getDocRef(colId: 'cardsStack' | 'cardsPlayed', docId: string) {
+    return doc(this.firestore, colId, docId);
+  }
+
+  setCardObject(cardSrc: any, cardId: string): Card {
+    return {
+      src: cardSrc,
+      ID: cardId
+    }
+  }
 }
